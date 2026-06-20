684a2c0708f739c9776b9ac9214cbcdf05e54c13
malikhan4050 <malikhan4050@gmail.com>
2026-06-20 07:50:58 +0500

    Added pwd commmand + cd with relative, absolute, and home directory support

--- app/main.py
+++ app/main.py
@@ -15,9 +15,11 @@ def main():
         if command == "exit":
             print("Goodbye!")
             sys.exit(0)
+
         elif command.startswith("show "):
             msg = command[5:]
             print(msg)
+        
         elif command.startswith("type "):
             cmd = command[5:].strip()
             builtinTypes = ["exit", "type", "show"]
@@ -30,6 +32,7 @@ def main():
                     print(f"{cmd} is a {path}")
                 else:
                     print(f"{cmd} is not found, sorry!")
+        
         elif command.startswith("kill "):
             process_name = command[5:].strip()
             try:
@@ -37,11 +40,27 @@ def main():
                 print(f"Closed all processes name {process_name}.exe")
             except Exception as e:
                 print(f"Error killing the process {process_name} : {e}")
+        
+        elif command == "pwd":
+            dir = os.getcwd()
+            print(f"The current working directory is : {dir}")
+        
+        elif command.startswith("cd "):
+            path = command[3:].strip()
+            try:
+                os.chdir(path)
+            except FileNotFoundError:
+                print(f"cd : {path} : no such file or directory")
+            except NotADirectoryError:
+                print(f"cd : {path} : Not a directory")
+            except Exception as e:
+                print(f"cd : {path} : {e}")
+        
         else:
             path = shutil.which(command)
             if path:
                 try:
-                    subprocess.run(command, shell=True)
+                    subprocess.Popen(command, shell=True)
                 except Exception as e:
                     print(f"Error executing the command: {e}")
             else:
